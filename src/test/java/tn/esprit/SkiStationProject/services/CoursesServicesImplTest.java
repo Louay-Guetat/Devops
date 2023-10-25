0da81ec0812650a31285b5c04502a78eb399852f
Louay-Guetat <louay.guetat@esprit.tn>
2023-10-25 15:41:21 +0100

    all tests are valid

--- src/test/java/tn/esprit/SkiStationProject/services/CoursesServicesImplTest.java
+++ src/test/java/tn/esprit/SkiStationProject/services/CoursesServicesImplTest.java
@@ -3,6 +3,7 @@ package tn.esprit.SkiStationProject.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Random;
 
 import javax.transaction.Transactional;
 
@@ -31,45 +32,85 @@ public class CoursesServicesImplTest {
 
     @Test
     @Order(1)
-    public void testRetrieveAllCourses(){
-        long rowCount = courseRepository.count();
-
+    public void testRetrieveAllCourses(){        
         List<Course> listCourses = courseServices.retrieveAllCourses();
-
-        Assertions.assertEquals(rowCount, listCourses.size());
+        Assertions.assertEquals(0, listCourses.size());
     }
 
     @Test
     @Order(2)
     public void testAddCourse(){
         Course course = new Course();
-        course.setLevel(2);
+        course.setLevel(1);
         course.setPrice(22.04F);
         course.setSupport(Support.SKI);
         course.setTimeSlot(200);
         course.setTypeCourse(TypeCourse.INDIVIDUAL);
 
         Course savedCourse = courseServices.addCourse(course);
-        assertEquals(course, savedCourse);
+        assertEquals(course.getId(), savedCourse.getId());
     }
 
     @Test
     @Order(3)
     public void testUpdateCourse() {
-        Course course = courseServices.retrieveCourse(5L);
+        List<Course> listCourses = courseServices.retrieveAllCourses();
+        Long id = 0L;
+        if (!listCourses.isEmpty()) {
+            // Initialize a Random object
+            Random random = new Random();
+        
+            // Generate a random index
+            int randomIndex = random.nextInt(listCourses.size());
+        
+            // Get the Course object at the random index
+            Course randomCourse = listCourses.get(randomIndex);
+        
+            id = randomCourse.getId();
+        }
+        Course course = courseServices.retrieveCourse(id);
         course.setPrice(780.5F);
         course.setTimeSlot(1000);
         
         Course updated = courseServices.updateCourse(course);
-        assertEquals(course, updated);
+        assertEquals(course.getId(), updated.getId());
     }
 
     @Test
     @Order(4)
     public void testRetrieveCourse() {
-        Long id = 5L;
+        List<Course> listCourses = courseServices.retrieveAllCourses();
+        Long id = 0L;
+        if (!listCourses.isEmpty()) {
+            // Initialize a Random object
+            Random random = new Random();
+        
+            // Generate a random index
+            int randomIndex = random.nextInt(listCourses.size());
+        
+            // Get the Course object at the random index
+            Course randomCourse = listCourses.get(randomIndex);
+        
+            id = randomCourse.getId();
+        }
         Course course = courseServices.retrieveCourse(id);
 
         assertEquals(id, course.getId());
+    }
+
+    @Test
+    @Order(5)
+    public void testVerifyCrud(){
+        List<Course> listCourses = courseServices.retrieveAllCourses();
+        Assertions.assertEquals(1, listCourses.size());
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteAllCourses(){
+        courseRepository.deleteAll();
+
+        List<Course> listCourses = courseServices.retrieveAllCourses();
+        Assertions.assertEquals(0, listCourses.size());
     }
 }
